34b0d56115c7a596c2ecc6ebcf8167715d0b474b
Shriya <shriya.atmak@gmail.com>
2022-05-05 17:55:11 -0400

    Changed place of caching

--- utils.py
+++ utils.py
@@ -35,35 +35,31 @@ def get_dataset(tokenizer, dataset_path, dataset_cache, max_history):
     """ Get tokenized PERSONACHAT dataset from S3 or cache."""
     dataset_path = dataset_path or QUAC_TRAIN_URL
     dataset_val_path = QUAC_VALID_URL
-    dataset_cache = dataset_cache + '_' + type(tokenizer).__name__  # To avoid using GPT cache for GPT-2 and vice-versa
-    if dataset_cache and os.path.isfile(dataset_cache):
-        logger.info("Load tokenized dataset from cache at %s", dataset_cache)
-        dataset = torch.load(dataset_cache)
-    else:
-        logger.info("Download dataset from %s", dataset_path)
-        #quac_file = cached_path(dataset_path)
-        
-        #with open(quac_file, "r", encoding="utf-8") as f:
-            #dataset = json.loads(f.read())
-        
-        if dataset_path == QUAC_TRAIN_URL:
-            dataset = load_dataset('quac', split='train')
-            dataset_val = load_dataset('quac', split='validation')
-            dataset, turnid_idx_map = flatten_data(dataset)
-            dataset_val, val_turnid_idx_map = flatten_data(dataset_val)
-            dataset = add_history(dataset, max_history, turnid_idx_map)
-            dataset_val = add_history(dataset_val, max_history, val_turnid_idx_map)
-            dataset = {'train' : dataset, 'valid' : dataset_val}
 
-        #logger.info("Tokenize and encode the dataset")
-        def tokenize(obj):
-            if isinstance(obj, str):
-                return tokenizer.convert_tokens_to_ids(tokenizer.tokenize(obj))
-            if isinstance(obj, dict):
-                return dict((n, tokenize(o)) for n, o in obj.items())
-            return list(tokenize(o) for o in obj)
-        #dataset = tokenize(dataset)
-        torch.save(dataset, dataset_cache)
+    logger.info("Download dataset from %s", dataset_path)
+    #quac_file = cached_path(dataset_path)
+    
+    #with open(quac_file, "r", encoding="utf-8") as f:
+        #dataset = json.loads(f.read())
+    
+    if dataset_path == QUAC_TRAIN_URL:
+        dataset = load_dataset('quac', split='train')
+        dataset_val = load_dataset('quac', split='validation')
+        dataset, turnid_idx_map = flatten_data(dataset)
+        dataset_val, val_turnid_idx_map = flatten_data(dataset_val)
+        dataset = add_history(dataset, max_history, turnid_idx_map)
+        dataset_val = add_history(dataset_val, max_history, val_turnid_idx_map)
+        dataset = {'train' : dataset, 'valid' : dataset_val}
+
+    #logger.info("Tokenize and encode the dataset")
+    def tokenize(obj):
+        if isinstance(obj, str):
+            return tokenizer.convert_tokens_to_ids(tokenizer.tokenize(obj))
+        if isinstance(obj, dict):
+            return dict((n, tokenize(o)) for n, o in obj.items())
+        return list(tokenize(o) for o in obj)
+    #dataset = tokenize(dataset)
+    
     return dataset
 
 def flatten_data(data):
